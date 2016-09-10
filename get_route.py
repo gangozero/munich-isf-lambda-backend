42e91d25fd930fc5aa6728a77e43bced9d557dfa
Andrey Prokopiev <andrey.prokopiev@gmail.com>
2016-09-10 11:11:13 +0200

    Add proper error handling for mapping in API Gateway

--- get_route.py
+++ get_route.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 
 import boto3
-import json
-
-print('Loading function')
 
 
 def lambda_handler(event, context):
@@ -33,32 +30,37 @@ def lambda_handler(event, context):
                     stop_resp = dyn_stops.get_item(Key={'id': s})
                     if stop_resp.has_key('Item') and stop_resp['Item']:
                         stop_data = stop_resp['Item']
-                        geo_stops.append({'name': stop_data['name'], 'lat': stop_data['lat'], 'lon': stop_data['lon']})
+                        geo_stops.append({'name': stop_data['name'],
+                                          'lat': float(stop_data['lat']),
+                                          'lon': float(stop_data['lon'])})
                     else:
-                        return "{ 'error': 'Location %s not found in DB'}" % s
+                        raise Exception("Bad Request: Location %s not found in DB" % s)
                 # Append start stop
                 answer.append({'type': 'start',
                                'name': geo_stops[0]['name'],
-                               'lat': geo_stops[0]['lat'],
-                               'lon': geo_stops[0]['lon']})
+                               'lat': float(geo_stops[0]['lat']),
+                               'lon': float(geo_stops[0]['lon'])})
 
                 # Append end stop
                 answer.append({'type': 'end',
                                'name': geo_stops[-1]['name'],
-                               'lat': geo_stops[-1]['lat'],
-                               'lon': geo_stops[-1]['lon']})
+                               'lat': float(geo_stops[-1]['lat']),
+                               'lon': float(geo_stops[-1]['lon'])})
 
                 # Check that number of time durations equal to number of remaining stops and iterate over them
                 if item.has_key('stop_time') and len(item['stop_time']) == len(geo_stops[1:-1]):
                     for m in geo_stops[1:-1]:
-                        answer.append({'type': 'middle', 'name': m['name'], 'lat': m['lat'], 'lon': m['lon']})
+                        answer.append({'type': 'middle',
+                                       'name': m['name'],
+                                       'lat': float(m['lat']),
+                                       'lon': float(m['lon'])})
 
-                    return json.dumps(answer)
+                    return answer
                 else:
-                    return "{ 'error': 'No stop duration times in DB or wrong number'}"
+                    raise Exception("Bad Request: No stop duration times in DB or wrong number")
             else:
-                return "{ 'error': 'Route has to include at least 2 stops'}"
+                raise Exception("Bad Request: Route has to include at least 2 stops")
         else:
-            return "{ 'error': 'Stop list not found in response'}"
+            raise Exception("Bad Request: Stop list not found in response")
     else:
-        return "{ 'error': 'Route not found'}"
+        raise Exception("Not Found: Route not found")
